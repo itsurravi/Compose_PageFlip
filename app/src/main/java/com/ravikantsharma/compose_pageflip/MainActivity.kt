9252f11322ca046749224c0f9081d6932b4133ad
Ravikant Sharma <ravikant.sharma@prepladder.com>
2024-06-03 11:45:01 +0530

    Initial commit

---
+++ app/src/main/java/com/ravikantsharma/compose_pageflip/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.ravikantsharma.compose_pageflip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ravikantsharma.compose_pageflip.ui.theme.Compose_PageFlipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Compose_PageFlipTheme {
+
+            }
+        }
+    }
+}
